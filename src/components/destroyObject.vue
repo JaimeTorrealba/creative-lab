3892e32dce4658fe21df7892cf26bc984c1c6c91
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-04-22 17:58:53 -0400

    finish the destroy effect, add cone geo

--- src/components/destroyObject.vue
+++ src/components/destroyObject.vue
@@ -1,108 +1,215 @@
 <script setup>
 import { shallowRef, onMounted } from 'vue'
-import { OrbitControls } from '@tresjs/cientos'
+import { PamCameraMouse, Text3D } from '@tresjs/cientos'
 import gsap from 'gsap'
-import { TresCanvas, useRenderLoop, useTexture } from '@tresjs/core'
-import { AdditiveBlending, Vector3, Spherical, BufferAttribute, Color } from 'three'
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
+import { SphereGeometry, BoxGeometry, BufferAttribute, ConeGeometry } from 'three'
 
-const pointsRef = shallowRef()
+const fontPath = 'https://raw.githubusercontent.com/Tresjs/assets/main/fonts/FiraCodeRegular.json'
 
-let squareColor = new Color()
-// squareColor.setHex( 0x4F4F4F);
-squareColor.setHex( 0x4f4);
-console.log('jaime ~ squareColor:', squareColor.g);
+const squareRef = shallowRef()
+const sphereRef = shallowRef()
+const triangleRef = shallowRef()
+
+const squareGeo = new BoxGeometry(1.5,1.5,1, 10, 10, 10).toNonIndexed()
+const sphereGeo = new SphereGeometry(1, 20, 20).toNonIndexed()
+const triangleGeo = new ConeGeometry(0.9, 1.5, 20, 20).toNonIndexed();
+
 
 onMounted(() => {
-  console.log(pointsRef.value.geometry)
-  pointsRef.value.geometry.setAttribute('position', new BufferAttribute(positionArray, 3))
-  pointsRef.value.geometry.setAttribute( 'color', new BufferAttribute( squareColorArray, 3 ));
+  squareRef.value.geometry.dispose()
+  squareRef.value.geometry = squareGeo
+  const lenSquare = squareRef.value.geometry.attributes.position.count
+  let squareRandoms = new Float32Array(lenSquare * 3)
+  for (let i = 0; i < lenSquare; i+= 3) {
+    let r = Math.random()
+    squareRandoms[i] = r
+    squareRandoms[i + 1] = r
+    squareRandoms[i + 2] = r
+  }
+  squareRef.value.geometry.setAttribute('aRandom', new BufferAttribute(squareRandoms, 1))
+
+  // sphere
+  sphereRef.value.geometry.dispose()
+  sphereRef.value.geometry = sphereGeo
+  const lenSphere = sphereRef.value.geometry.attributes.position.count
+
+  let sphereRandoms = new Float32Array(lenSphere * 3)
+  for (let i = 0; i < lenSquare; i+= 3) {
+    let r = Math.random()
+    sphereRandoms[i] = r
+    sphereRandoms[i + 1] = r
+    sphereRandoms[i + 2] = r
+  }
+  sphereRef.value.geometry.setAttribute('aRandom', new BufferAttribute(sphereRandoms, 1))
+
+  // triangle
+   triangleRef.value.geometry.dispose()
+   triangleRef.value.geometry = triangleGeo
+  const lenTriangle = triangleRef.value.geometry.attributes.position.count
+
+  let triangleRandoms = new Float32Array(lenTriangle * 3)
+  for (let i = 0; i < lenSquare; i+= 3) {
+    let r = Math.random()
+    triangleRandoms[i] = r
+    triangleRandoms[i + 1] = r
+    triangleRandoms[i + 2] = r
+  }
+  triangleRef.value.geometry.setAttribute('aRandom', new BufferAttribute(triangleRandoms, 1))
+
 })
 
-
-
-const pointsOptions = {
-  size: 0.2,
-  vertexColors: true,
-  transparent: false,
-  blending: AdditiveBlending,
-}
-
-const count = 100000
-
-const positionArray = new Float32Array(count * 3)
-const squareColorArray = new Float32Array(count * 3)
-
-for (let i = 0; i < count; i++) {
-  // positionArray[i * 3 + 0] = Math.random() - 0.5
-  // positionArray[i * 3 + 1] = Math.random()
-  // positionArray[i * 3 + 2] = Math.random() - 0.5
-  positionArray[i] = (Math.random() - 0.5)
-  squareColorArray[i *3 ] = squareColor.r
-  squareColorArray[i *3 + 1] = squareColor.g
-  squareColorArray[i *3 + 2] = squareColor.b
-
-  // squareColorArray[i * 3 + 0] = squareColor.r
-  // squareColorArray[i * 3 + 1] = squareColor.g
-  // squareColorArray[i * 3 + 2] = squareColor.b
-}
-
-const shader = {
-  transparent: false,
-  blending: AdditiveBlending,
-  depthWrite: false,
-  size: 0.2,
-  vertexColors: true,
-
+const squareShader = {
   vertexShader: `
-    varying vec3 vColor;
+  attribute vec3 aRandom;
+  uniform float uTime;
+  uniform float uProgress;
     void main() {
-      vColor = color;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      vec3 newPos = position;
+      newPos.x += (aRandom.x * uv.x -0.5)* uProgress * 2.0;
+      newPos.y += (aRandom.x * uv.y -0.5)* uProgress * 2.0;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( newPos, 1.0 );
     }
   `,
   fragmentShader: `
-    varying vec3 vColor;
+  uniform float uProgress;
     void main() {
-      gl_FragColor = vec4(vColor, 1.0);
+      gl_FragColor = vec4(0.3,0.3,0.3, 1.0 - uProgress);
     }`,
+  uniforms: {
+    uTime: { value: 0 },
+    uProgress: { value: 0 } // cambiar a 0
+  },
+  transparent:true
 }
+const sphereShader = {
+  vertexShader: `
+  attribute float aRandom;
+  uniform float uTime;
+  uniform float uProgress;
+    void main() {
+      vec3 newPos = position;
+      newPos += (aRandom * normal) * uProgress * 2.0;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( newPos, 1.0 );
+    }
+  `,
+  fragmentShader: `
+  uniform float uProgress;
+    void main() {
+      gl_FragColor = vec4(0.9,0.69,0.23, 1.0 - uProgress);
+    }`,
+  uniforms: {
+    uTime: { value: 0 },
+    uProgress: { value: 0 } // cambiar a 0
+  },
+  transparent:true
+}
+
+const triangleShader = {
+  vertexShader: `
+  attribute vec3 aRandom;
+  uniform float uTime;
+  uniform float uProgress;
+    void main() {
+      vec3 newPos = position;
+      //newPos.xy += (aRandom.xy * uv.xy -0.5)* 1.0 * 2.0;
+      // newPos.y += (aRandom.x * uv.y -0.5)* uProgress * 2.0;
+      //newPos += (aRandom * normal) * 1.0 * 2.0;
+      newPos.x += (aRandom.x * uv.x -0.5)* uProgress * 2.0;
+      newPos.y += (aRandom.x * uv.y -0.5)* uProgress * 2.0;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( newPos, 1.0 );
+    }
+  `,
+  fragmentShader: `
+  uniform float uProgress;
+    void main() {
+      gl_FragColor = vec4(0.5,0.85,0.77, 1.0 - uProgress);
+    }`,
+  uniforms: {
+    uTime: { value: 0 },
+    uProgress: { value: 0 }
+  },
+  transparent:true
+}
+
+const { onLoop } = useRenderLoop()
+onLoop(() => {
+  squareShader.uniforms.uTime.value += 0.01
+  sphereShader.uniforms.uTime.value += 0.01
+  triangleShader.uniforms.uTime.value += 0.01
+})
 
 const destroySquare = () => {
-  const squareGeo = pointsRef.value.geometry.attributes.position
-
-  if (squareGeo.count === 0) {
-    gsap.to(squareGeo, {
-      count: count,
-      duration: 3
-    })
-    return
-  }
-
-  gsap.to(squareGeo, {
-    count: 0,
-    duration: 1
+  gsap.to(squareShader.uniforms.uProgress, {
+    value: 1,
+    duration: 1,
+    ease: 'power4.in',
   })
 }
+const destroySphere = () => {
+  gsap.to(sphereShader.uniforms.uProgress, {
+    value: 1,
+    duration: 1,
+    ease: 'power4.in',
+  })
+}
+const destroyTriangle = () => {
+  gsap.to(triangleShader.uniforms.uProgress, {
+    value: 1,
+    duration: 1,
+    ease: 'power4.in',
+  })
+}
+const destroyAll = () => {
+  destroySquare()
+  destroySphere()
+  destroyTriangle()
+}
+const restore = () => {
+    gsap.to([
+    squareShader.uniforms.uProgress,
+    sphereShader.uniforms.uProgress,
+    triangleShader.uniforms.uProgress
+  ], {
+    value: 0,
+    duration: 1,
+    ease: 'back',
+    })
+  }
 </script>
 <template>
-  <button @click="destroySquare()">go wild</button>
+  <div class="is-flex is-justify-content-center">
+    <button class="mr-2 my-2 button is-danger" @click="destroyAll()">Destroy All</button>
+    <button class="mr-2 my-2 button is-success" @click="restore()">Restore</button>
+  </div>
   <Suspense>
-    <TresCanvas window-size clear-color="#f7f7f7">
+    <TresCanvas window-size clear-color="#000">
       <TresPerspectiveCamera
-        :args="[1, 32, 32]"
-        :position="[0, 0, 5]"
+        :position="[0, 0, 7.5]"
         :fov="45"
         :aspect="1"
         :near="0.1"
         :far="1000"
+        :look-at="[0, 0, 0]"
       />
-      <OrbitControls />
-      <TresPoints ref="pointsRef">
-        <!-- <TresBufferGeometry :position="[positionArray, 3]" /> -->
-        <TresShaderMaterial v-bind="shader" />
-      </TresPoints>
+      <Suspense>
+        <Text3D :font="fontPath" :position="[0, 2, 0 ]" center />
+      </Suspense>
+      <TresMesh ref="squareRef" @click="destroySquare()">
+        <TresBoxGeometry :args="[1, 1, 1, 10, 10, 10]" />
+        <TresShaderMaterial v-bind="squareShader" />
+      </TresMesh>
+      <TresMesh ref="sphereRef" :position="[3, 0, 0]" @click="destroySphere()">
+        <TresIcosahedronGeometry :args="[0.75, 10]"  />
+        <TresShaderMaterial v-bind="sphereShader" />
+      </TresMesh>
+      <TresMesh ref="triangleRef" :position="[-3, 0, 0]" @click="destroyTriangle()">
+        <TresConeGeometry :args="[1, 1.5, 60, 60]"  />
+        <TresShaderMaterial v-bind="triangleShader" />
+      </TresMesh>
+      <PamCameraMouse :factor="0.5" />
       <TresGridHelper :args="[30, 30]" :position="[0, -2.5, 0]" />
-      <TresDirectionalLight :position="[0, 2, 4]" :intensity="2" cast-shadow />
+      <TresDirectionalLight :position="[0, 2, 4]" :intensity="2" />
       <TresAmbientLight />
     </TresCanvas>
   </Suspense>
