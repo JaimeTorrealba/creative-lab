9431b692d8cebbffa90e9a3182a6d0a9fc922837
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-04-22 18:30:39 -0400

    test broken component

--- src/router/index.js
+++ src/router/index.js
@@ -5,47 +5,47 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'Home', component: Home },
-    {
-      path: '/DynamicTextEffect',
-      name: 'DynamicTextEffect',
-      component: () => import('../views/DynamicTextEffect.vue')
-    },
-    {
-      path: '/multiCamera',
-      name: 'MultiCamera',
-      component: () => import('../views/MultiCamera.vue')
-    },
-    {
-      path: '/glassmorphism',
-      name: 'GlassMorphism',
-      component: () => import('../views/GlassMorphism.vue')
-    },
-    {
-      path: '/mixHtml',
-      name: 'MixHTML',
-      component: () => import('../views/MixHTMLView.vue')
-    },
-    {
-      path: '/destroyObject',
-      name: 'DestroyObject',
-      component: () => import('../views/DestroyGeosView.vue')
-    },
-    // Shaders
-    {
-      path: '/MouseShaderEffect',
-      name: 'MouseShaderEffect',
-      component: () => import('../views/MouseShaderEffect.vue')
-    },
-    {
-      path: '/darkStar',
-      name: 'DarkStar',
-      component: () => import('../views/DarkStarView.vue')
-    },
-    {
-      path: '/shaderParkIntegration',
-      name: 'ShaderParkIntegration',
-      component: () => import('../views/ShaderParkIntegrationView.vue')
-    }
+    // {
+    //   path: '/DynamicTextEffect',
+    //   name: 'DynamicTextEffect',
+    //   component: () => import('../views/DynamicTextEffect.vue')
+    // },
+    // {
+    //   path: '/multiCamera',
+    //   name: 'MultiCamera',
+    //   component: () => import('../views/MultiCamera.vue')
+    // },
+    // {
+    //   path: '/glassmorphism',
+    //   name: 'GlassMorphism',
+    //   component: () => import('../views/GlassMorphism.vue')
+    // },
+    // {
+    //   path: '/mixHtml',
+    //   name: 'MixHTML',
+    //   component: () => import('../views/MixHTMLView.vue')
+    // },
+    // {
+    //   path: '/destroyObject',
+    //   name: 'DestroyObject',
+    //   component: () => import('../views/DestroyGeosView.vue')
+    // },
+    // // Shaders
+    // {
+    //   path: '/MouseShaderEffect',
+    //   name: 'MouseShaderEffect',
+    //   component: () => import('../views/MouseShaderEffect.vue')
+    // },
+    // {
+    //   path: '/darkStar',
+    //   name: 'DarkStar',
+    //   component: () => import('../views/DarkStarView.vue')
+    // },
+    // {
+    //   path: '/shaderParkIntegration',
+    //   name: 'ShaderParkIntegration',
+    //   component: () => import('../views/ShaderParkIntegrationView.vue')
+    // }
     // { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
   ]
 })
