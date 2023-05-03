539bcb5861a6d04ad17be1bc4520f9608c148a08
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-05-03 10:29:14 -0400

    test: SP integration

--- src/components/shaders_experiments/shaderParkIntegration/spCode.js
+++ src/components/shaders_experiments/shaderParkIntegration/spCode.js
@@ -1,12 +1,10 @@
 /* eslint-disable */
 export function spCode() {
-  color(0, 0, 1)
   metal(0.9)
   shine(0.7)
   displace(0, 0, 0)
   sphere(0.3)
 
-  color(0, 0, 1)
   displace(mouse.x, mouse.y, 0)
   metal(0.9)
   blend(0.2)
