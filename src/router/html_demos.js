0306356b93d095f131372c558ef82bc1bb6f6be9
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-07-18 16:11:26 -0400

    add reactive text demo

--- src/router/html_demos.js
+++ src/router/html_demos.js
@@ -7,8 +7,8 @@ export const html_demos = [
     meta: {
       layout: defaultLayout,
       name: 'A basic HTML-TresJs Mix',
-      difficulty: 'mesium',
-      isOnTres: true,
+      difficulty: 'medium',
+      isOnTres: false,
       section: 'html_demo',
       img: '/gifs/mixhtml_demo.gif',
       sourceCode:
@@ -17,5 +17,22 @@ export const html_demos = [
         'A basic example of how to mix TresJs with some HTML. I use a camera config to make the units in ThreeJs match the pixels in HTML. Some hover effects on the image and some 3D icons. Also a simple fragment shader banner '
     },
     component: () => import('../views/html_demos/MixHTMLDemo.vue')
+  },
+  {
+    path: '/dynamic_text_demo',
+    name: 'Dinamic text Demo',
+    meta: {
+      layout: defaultLayout,
+      name: 'A reactive 3D text',
+      difficulty: 'Easy',
+      isOnTres: true,
+      section: 'html_demo',
+      img: '/gifs/reactive3Dtext.gif',
+      sourceCode:
+        'https://github.com/JaimeTorrealba/creative-lab/blob/main/src/views/html_demos/DynamicTextDemo.vue',
+      description:
+        'A basic example of a login with 3D reactive text using VueJs and TresJs.'
+    },
+    component: () => import('../views/html_demos/DynamicTextDemo.vue')
   }
 ]
