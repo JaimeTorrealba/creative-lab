e0121a33625cc369cb776875e50c882c221b98a1
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-07-19 19:46:50 -0400

    add favicon, rrss, gsap scroll in home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 import Cards from '@/components/TheCard.vue'
 
 const router = useRouter()
@@ -8,116 +11,57 @@ const rawData = computed(() => router.options.routes)
 
 const data = rawData.value.filter((route) => route.path !== '/')
 
-// const inspirations = [
-//   {
-//     name: 'Frontend Horse',
-//     link: 'https://www.youtube.com/@TrostCodes'
-//   },
-//   {
-//     name: 'Bruno Simon',
-//     link: 'https://threejs-journey.com/'
-//   },
-//   {
-//     name: 'Yuri Artyukh',
-//     link: 'https://www.youtube.com/@akella_'
-//   },
-//   {
-//     name: 'Gsap showcase',
-//     link: 'https://greensock.com/showcase/'
-//   },
-//   {
-//     name: 'Awwwards',
-//     link: 'https://www.awwwards.com/'
-//   },
-//   {
-//     name: 'Dribble',
-//     link: 'https://dribbble.com/'
-//   },
-//   {
-//     name: 'React three (ecosystem)',
-//     link: 'https://github.com/pmndrs'
-//   },
-//   {
-//     name: 'Codepen',
-//     link: 'https://codepen.io'
-//   }
-// ]
-// const resources = [
-//   {
-//     name: 'Threejs',
-//     link: 'https://threejs.org/'
-//   },
-//   {
-//     name: 'TresJs (ecosystem)',
-//     link: 'https://tresjs.org/'
-//   },
-//   {
-//     name: '3dtextures (textures)',
-//     link: 'https://3dtextures.me/'
-//   },
-//   {
-//     name: 'withpoly (textures)',
-//     link: 'https://withpoly.com/'
-//   },
-//   {
-//     name: 'Polyhaven (textures)',
-//     link: 'https://polyhaven.com/'
-//   },
-//   {
-//     name: 'Poly.pizza (models)',
-//     link: 'https://poly.pizza/'
-//   },
-//   {
-//     name: 'Sketchfab (models)',
-//     link: 'https://sketchfab.com/'
-//   }
-// ]
+onMounted(() => {
+  const scrollTriggerRef = gsap.utils.toArray('.scrollTriggerRef')
+  scrollTriggerRef.map((card) => {
+    ScrollTrigger.create({
+      trigger: card,
+      start: 'top 75%',
+      animation: gsap.from(card, { duration: 0.75, opacity: 0 })
+    })
+  })
+})
 </script>
 <template>
   <v-container class="pa-16 bg">
     <h1 class="text-center text-h3">Welcome to my creative lab</h1>
-    <v-list ref="navigationRef">
-      <v-list-item
-        prepend-avatar="https://avatars.githubusercontent.com/u/63722373?v=4"
-        title="Jaime Torrealba"
-        subtitle="@jaimebboyjt"
-      >
-      </v-list-item>
-    </v-list>
     <v-row class="pa-8" flex justify="space-around">
       <v-col cols="4" v-for="route in data" :key="route.path">
-        <Cards :data="route" v-if="route.path !== '/'" />
+        <Cards :data="route" v-if="route.path !== '/'" class="scrollTriggerRef" />
       </v-col>
     </v-row>
+    <v-footer class="text-center d-flex flex-column rounded">
+      <div class="d-flex align-center">
+        <a href="https://twitter.com/jaimebboyjt" rel="noreferrer" target="_blank" class="mx-4">
+          <img cover src="/images/twitter-logo.svg" />
+        </a>
+        <a href="https://github.com/JaimeTorrealba" rel="noreferrer" target="_blank" class="mx-4">
+          <img src="/images/github-logo.svg" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jaime-torrealba-cordova/"
+          rel="noreferrer"
+          target="_blank"
+          class="mx-4"
+        >
+          <img src="/images/Linkedin-logo.svg" />
+        </a>
+        <a href="https://codepen.io/jaime_torrealba" rel="noreferrer" target="_blank" class="mx-4">
+          <img src="/images/codepen-logo.svg" />
+        </a>
+      </div>
+
+      <div class="pt-0">
+        Big thanks to all of my inspirations and resources. <br />
+        Feel free to take any code that you want here, and if you have a doubt or suggestion, please
+        contact me on my social networks.
+      </div>
+
+      <v-divider></v-divider>
+
+      <div>{{ new Date().getFullYear() }} — <strong>MIT License</strong></div>
+    </v-footer>
   </v-container>
-  <!-- <v-row class="pa-8">
-      <v-col>
-        <h3 class="text-h5 mb-4">
-          I take several inspiration from several resource here are some of them:
-        </h3>
-          <v-list style="background-color: #222;">
-            <v-list-item v-for="{ name, link } in inspirations" :key="name">
-              <a :href="link" target="_blank">
-                <v-list-item-title v-text="name"></v-list-item-title>
-              </a>
-            </v-list-item>
-            <v-list-item style="color: #f7f7f7;">To all thanks</v-list-item>
-          </v-list>
-      </v-col>
-      <v-col>
-        <h3 class="text-h5 mb-4">Also I use several models/code/textures</h3>
-        <v-list-item v-for="{ name, link } in resources" :key="name">
-          <a :href="link" target="_blank">
-            <v-list-item-title v-text="name"></v-list-item-title>
-          </a>
-        </v-list-item>
-      </v-col>
-    </v-row>
-    <v-row class="pa-8">
-      <v-col class="d-flex justify-center">
-        <v-btn to="/contactme" variant="outlined">Go to Contact me</v-btn>
-      </v-col>
-    </v-row> -->
 </template>
 <style scoped>
 h1,
