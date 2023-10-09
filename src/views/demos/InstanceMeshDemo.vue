ffb6d85a33d71dd0a408c81ff47eb1bc43aa7737
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-10-09 15:22:28 -0300

    add matcap to isntanceMesh

--- src/views/demos/InstanceMeshDemo.vue
+++ src/views/demos/InstanceMeshDemo.vue
@@ -1,34 +1,54 @@
 <script setup>
 import { shallowRef, watch } from 'vue'
-import { TresCanvas, useRenderLoop } from '@tresjs/core'
+import { TresCanvas, useRenderLoop, useTexture } from '@tresjs/core'
 import { BoxGeometry, ShaderMaterial, Object3D, InstancedBufferAttribute } from 'three'
 import { MapControls } from '@tresjs/cientos'
 import { useWindowSize } from '@vueuse/core'
 
 const { width, height } = useWindowSize()
+const { matcap } = await useTexture({
+  matcap: '/textures/matcap example.png',
+})
+console.log('jaime ~ matcap:', matcap);
 
 const dummy = new Object3D();
 const material = new ShaderMaterial({
   uniforms: {
     uTime: { value: 0 },
+    uMatCap: { value: matcap },
   },
   vertexShader: `
     uniform float uTime;
     varying vec2 vUv;
+    varying vec3 vViewPosition;
+    varying vec3 vNormal;
     attribute float aRandom;
     void main() {
-      vUv = uv;
       vec4 modelPosition = modelMatrix * instanceMatrix * vec4(position, 1.0);
       // modelPosition.y += aRandom * sin(uTime + modelPosition.z * 10.0) * 0.1;
       modelPosition.y += aRandom * sin(uTime + 15.*aRandom) * 0.4;
       modelPosition = viewMatrix * modelPosition;
       gl_Position = projectionMatrix * modelPosition;
+      vUv = uv;
+      vViewPosition = -modelPosition.xyz;
+      // normal
+      vNormal = normalMatrix * mat3(instanceMatrix) * normal;
     }
   `,
   fragmentShader: `
+    uniform sampler2D uMatCap;
     varying vec2 vUv;
+    varying vec3 vViewPosition;
+    varying vec3 vNormal;
     void main() {
-      gl_FragColor = vec4(vUv.x, vUv.y, 0.0, 1.0);
+      vec3 normal = normalize(vNormal);
+      vec3 viewDir = normalize(vViewPosition);
+      vec3 x = normalize(vec3(viewDir.z, 0.0, -viewDir.x));
+      vec3 y = cross(viewDir, x);
+      vec2 uv = vec2(dot(x, normal), dot(y, normal)) * 0.495 + 0.5;
+      vec4 matcapColor = texture2D(uMatCap,uv);
+      // gl_FragColor = vec4(vNormal, 1);
+      gl_FragColor = matcapColor;
     }
   `,
 })
@@ -63,7 +83,7 @@ onLoop(({delta}) => {
 </script>
 <template>
   <TresCanvas window-size clear-color="#82DBC5">
-    <TresOrthographicCamera :args="[width / - 2, width / 2, height / 2, height / - 2, 1, 1000 ]" :position="[0, 2, 7.0]" :zoom="32" :look-at="[0,0,0]" />
+    <TresOrthographicCamera :args="[width / - 2, width / 2, height / 2, height / - 2, 1, 1000]" :position="[0, 2, 7.0]" :zoom="32" :look-at="[0,0,0]" />
     <MapControls />
     <TresInstancedMesh ref="instanceMeshRef" :args="[geometry, material, count]" />
   </TresCanvas>
