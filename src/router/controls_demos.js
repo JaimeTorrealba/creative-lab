ab662977b21ce5ba4e7c5c451316a09cea6451ce
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-01-11 15:46:45 +0000

    add click controls demo

--- src/router/controls_demos.js
+++ src/router/controls_demos.js
@@ -2,11 +2,13 @@ import { generateRoute } from '../utils'
 
 const descriptions = {
   thirdPerson: 'A third person camera controls, with glb model + animations, based on this video: https://youtu.be/C3s0UHpwlf8?si=JdxnmkFwgAxoOdk7',
+  click: 'A click base controls, with glb model + animations',
   sprites: 'A fusion between 2D and 3D world characters using sprites and the third person camera. based on this video: https://youtu.be/pGO1Hm-JB90?si=DUtUlYphEuiRr212',
 }
 
 export const controls_demos = [
   generateRoute('ThirdPerson', 'Controls_demos', descriptions.thirdPerson),
+  generateRoute('Click', 'Controls_demos', descriptions.click),
   generateRoute('Sprites', 'Controls_demos', descriptions.sprites),
   generateRoute('FPS', 'Controls_demos', ''),
 ]
---
+++ src/views/Controls_demos/Click.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { ref, shallowRef } from 'vue'
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
+import { OrbitControls, useGLTF, useAnimations } from '@tresjs/cientos'
+import { Vector3, DoubleSide } from 'three'
+
+const { scene: model, animations } = await useGLTF('/models/footman/source/Footman_RIG.glb')
+const { actions, mixer } = useAnimations(animations, model)
+
+//constants
+const fadeDuration = 0.2
+
+// template ref
+const cameraRef = shallowRef()
+const cameraTarget = new Vector3()
+const orbitControlsRef = shallowRef()
+const highlighter = shallowRef(null)
+const waitForAnimation = ref(false)
+const destination = ref(new Vector3())
+
+const curruentAction = ref(actions.Idle)
+curruentAction.value.play()
+
+const changeAnimation = (action) => {
+  curruentAction.value.fadeOut(fadeDuration)
+  action.reset().fadeIn(fadeDuration).play()
+  curruentAction.value = action
+  if (action === actions.SwordAndShieldSlash) {
+    mixer.addEventListener('loop', () => {
+      changeAnimation(actions.Idle)
+      waitForAnimation.value = false
+    })
+  }
+}
+
+const onSelect = (e) => {
+  const highlighterPos = new Vector3().copy(e.point).floor().addScalar(0.5)
+  highlighter.value.position.set(highlighterPos.x, 0, highlighterPos.z)
+  highlighter.value.visible = true
+}
+
+const moveCharacter = () => {
+  destination.value.x = highlighter.value.position.x
+  destination.value.y = 0
+  destination.value.z = highlighter.value.position.z
+  model.lookAt(destination.value)
+  changeAnimation(actions.SwordAndShieldRun)
+  setTimeout(() => {
+    changeAnimation(actions.Idle)
+  }, 500)
+}
+const { onLoop } = useRenderLoop()
+
+onLoop(({ delta }) => {
+  mixer.update(delta * 0.5)
+  orbitControlsRef.value.value.update()
+
+  model.position.x += (destination.value.x - model.position.x) * 0.1 * 0.5
+  model.position.z += (destination.value.z - model.position.z) * 0.1 * 0.5
+
+  // move camera
+  cameraRef.value.position.x += (destination.value.x - model.position.x) * 0.1 * 0.5
+  cameraRef.value.position.z += (destination.value.z - model.position.z) * 0.1 * 0.5
+  cameraTarget.x = model.position.x
+  cameraTarget.y = model.position.y
+  cameraTarget.z = model.position.z
+  orbitControlsRef.value.value.target = cameraTarget
+})
+
+</script>
+<template>
+  <TresCanvas window-size clear-color="#111" ref="canvasRef">
+    <TresPerspectiveCamera ref="cameraRef" :args="[45, width / height, 0.1, 1000]" :position="[0, 5, 5]" />
+    <OrbitControls enableDamping :enable-pan="false" :min-distance="5" :max-distance="15"
+      :max-polar-angle="Math.PI / 2 - 0.05" ref="orbitControlsRef" />
+    <primitive :object="model" />
+    <!-- GRID -->
+    <TresMesh ref="highlighter" :position="[0.5, 0, 0.5]" :rotation-x="-Math.PI * 0.5">
+      <TresPlaneGeometry :args="[1, 1]" />
+      <TresMeshBasicMaterial :color="0xf7f7f7" :side="DoubleSide" />
+    </TresMesh>
+    <TresMesh name="ground" :rotation-x="-Math.PI * 0.5" :visible="false" @click="moveCharacter()"
+      @pointer-move="(e) => onSelect(e)">
+      <TresPlaneGeometry :args="[10, 10, 32]" />
+      <TresMeshBasicMaterial :color="0x00ff00" :side="DoubleSide" />
+    </TresMesh>
+    <TresGridHelper :size="5" :divisions="10" />
+    <TresDirectionalLight :position="[0, 2, 4]" :intensity="2" />
+    <TresAmbientLight />
+  </TresCanvas>
+</template>
