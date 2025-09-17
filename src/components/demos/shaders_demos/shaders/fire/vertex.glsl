dfe8d37b14670a16ed95256146c27082a216dae8
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2025-09-17 21:57:17 +0100

    ♻️ Update fire demo, adding tweak pane, occlusion and now we can move it freely in any position without changing the shader

--- src/components/demos/shaders_demos/shaders/fire/vertex.glsl
+++ src/components/demos/shaders_demos/shaders/fire/vertex.glsl
@@ -1,5 +1,7 @@
-varying vec3 vWorldPos;
- void main() {
+varying vec3 vLocalPos;
+
+void main() {
+    // Pass object-space vertex position to the fragment shader
+    vLocalPos = position;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    vWorldPos = (modelMatrix * vec4(position, 1.0)).xyz;
 }
--- src/views/Shaders_demos/FireView.vue
+++ src/views/Shaders_demos/FireView.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { TresCanvas } from '@tresjs/core'
 import TheExperience from '@/components/demos/shaders_demos/FireDemo.vue'
-
+import { OrbitControls } from '@tresjs/cientos'
 </script>
 <template>
   <TresCanvas window-size clear-color="#111">
     <TresPerspectiveCamera :position="[0, 0, 3]"  />
+    <OrbitControls />
     <Suspense>
       <TheExperience />
     </Suspense>
