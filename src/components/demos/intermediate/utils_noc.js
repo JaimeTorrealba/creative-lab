381f0fca789f7a62a09de2d82ca722ab1b75fcb1
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2026-06-25 22:49:42 +0100

    🔥 stupid AI didn't delete the older files

--- src/components/demos/intermediate/utils_noc.js
+++
@@ -1,6 +0,0 @@
-export const convertToScreenCoords = (vector, width, height) => {
-
-  const centeredX = vector.x + width.value / 2;
-  const centeredY = height.value / 2 - vector.y; // flip Y so +Y is up
-  vector.set(centeredX, centeredY);
-}
